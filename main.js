95e1cdeaaed51550d252b4dee8b7e8f949733c97
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-10-31 15:07:15 +0100

    salvataggio

---
+++ main.js
@@ -0,0 +1,16 @@
+const mail = ["booleanError@gmail.com", "boolean@gmail.com", "booleanError@gmail.com"]
+console.log (mail)
+
+
+const verifica = prompt("Inserisci la mail")
+console.log( mail[verifica] );
+
+for (let i = 0; i < mail.length; i++) {
+    
+    const verifica = mail[i];
+    
+    if (mail == "boolean@gmail.com") {
+        
+        console.log("yes")
+    }
+}
