3a3d1f12e14fc3f5dcdbe803d7dd7f8420f73f72
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-10-31 15:36:12 +0100

    salavataggio

--- main.js
+++ main.js
@@ -9,10 +9,14 @@ console.log( mail[verifica] );
 for (let i = 0; i < mail.length; i++) {
     
     const verifica = mail[i];
-    console.log( verifica )
+    
     
     if (verifica == "boolean@gmail.com") {
         
         console.log("yes")
+    
+    } else if (verifica == "booleanError@gmail.com"){
+         
+        console.log("")
     }
 }
